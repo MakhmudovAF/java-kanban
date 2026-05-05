f2afcff8ea08221f54b3554052cbbf5e20aa1cbc
Amir Makhmudov <amir.makhmudov.f@gmail.com>
2026-05-05 11:56:26 +0300

    feat: создать HttpTaskServer с базовой конфигурацией
    - Запуск сервера на порту 8080 через HttpServer
    - Регистрация контекстов для /tasks, /epics, /subtasks
    - Инициализация Gson с адаптерами
    - Методы start()/stop() для управления жизненным циклом

---
+++ src/http/HttpTaskServer.java
@@ -0,0 +1,50 @@
+package http;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.sun.net.httpserver.HttpServer;
+import http.handler.*;
+import manager.Managers;
+import manager.TaskManager;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class HttpTaskServer {
+    private static final int PORT = 8080;
+    private final HttpServer httpServer;
+    private final Gson gson;
+
+    public HttpTaskServer(TaskManager manager) throws IOException {
+        this.gson = new GsonBuilder()
+                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+                .registerTypeAdapter(Duration.class, new DurationAdapter())
+                .create();
+
+        httpServer = HttpServer.create(new InetSocketAddress(PORT), 0);
+
+        httpServer.createContext("/tasks", new TaskHandler(manager, gson));
+        httpServer.createContext("/subtasks", new SubtaskHandler(manager, gson));
+        httpServer.createContext("/epics", new EpicHandler(manager, gson));
+        httpServer.createContext("/history", new HistoryHandler(manager, gson));
+        httpServer.createContext("/prioritized", new PrioritizedHandler(manager, gson));
+    }
+
+    public void start() {
+        httpServer.start();
+        System.out.println("HTTP-сервер запущен на порту " + PORT);
+    }
+
+    public void stop() {
+        httpServer.stop(0);
+        System.out.println("HTTP-сервер остановлен");
+    }
+
+    public static void main(String[] args) throws IOException {
+        TaskManager manager = Managers.getDefault();
+        HttpTaskServer server = new HttpTaskServer(manager);
+        server.start();
+    }
+}
